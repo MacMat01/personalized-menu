17f13ea68dc4c4aaca8a49c31283af97b79f26ca
Matteo Machella <matteo.machella01@gmail.com>
2025-04-15 09:32:49 +0200

    Update Prolog Exercise 1.pl

--- Assignments/Decision in the Admission Process/Prolog Exercise 1.pl
+++ Assignments/Decision in the Admission Process/Prolog Exercise 1.pl
@@ -1,25 +1,24 @@
-% Facts
+% FACTS
 
 % Persons and courses
 person(knut).
-course(kebi).
+course("KEBI").
 
 % Study programs: bachelor and master are declared.
 study_program(bachelor).
 study_program(master).
 
 % Computer science is a master program.
-% (Here we indicate that computer_science is a study program with level master.)
 program_level(computer_science, master).
 
 % The course «KEBI» belongs to the computer science study program.
-course_in_program(kebi, computer_science).
+course_in_program("KEBI", computer_science).
 
 % Teaching and attendance relationships
-teach_course(knut, kebi).   
-attend_course(john, kebi).  
+teach_course(knut, "KEBI").   
+attend_course(john, "KEBI").  
 
-% Rules
+% RULES
 
 % A student is a master student if they attend a course that belongs
 % to a study program whose level is master.
